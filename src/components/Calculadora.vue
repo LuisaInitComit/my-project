ad8a46c88f55d1113a0d5ee0ab6a9a916c930cdb
Luisa Carmona <cp.carmonaclf@gmail.com>
2021-09-14 10:22:19 -0500

    metodos

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -3,11 +3,17 @@
       <h1>Mi primer componente</h1>
       <!----Interpolación-->
       <h2>Calculadora</h2>
-      <input type="number" id="num1" placeholder="numero_1"/>
-      <br>
-      <input type="number" ide="num2" placeholder="numero_2"/>
-      <br>
-      <!---<button @click="">Sumar</button>-->
+      <input type="number" id="num1" placeholder="numero_1" v-model="numero_1"/>
+      <br />
+      <br />
+      <input type="number" ide="num2" placeholder="numero_2" v-model="numero_2"/>
+      <br/>
+      <button @click="sumar(numero_1, numero_2)">Sumar</button>
+      <button @click="multiplicar(numero_1, numero_2)">Multiplicar</button>
+      <button @click="dividir(numero_1, numero_2)">Dividir</button>
+      <br />
+      <br />
+      <span id="resultado">Resultado: {{resultado}}</span>
   </div>
 </template>
 
@@ -18,22 +24,36 @@ export default {
         return{
             subtitulo: 'Calculadora',
             numero_1: null,
-            numero_2: null
-        }
+            numero_2: null,
+            resultado: "",
+        };
     },
     methods:{
-        sumar(){
-            console.log("metodo sumer")
+        sumar(numero_1, numero_2){
+            console.log("metodo sumar")
+            console.log("Concatena: ", numero_1 + numero_2)
+            //sumar valores
+            this.resultado = parseInt(numero_1)+parseInt(numero_2)
+        },
+        multiplicar(){
+            console.log("metodo multiplicar")
             console.log("Resultado: ", (numero_1, numero_2))
-        }
+            //multiplicar valores
+            this.resultado = parseFloat(this.numero_1)*parseFloat(this.numero_2)
+        },
+        dividir(){
+            if(parseFloat(this.numero_2)==0){
+                alert("No es posible realizar la división");
+            } else{
+                this.resultado= parseFloat(this.numero_1)/parseFloat(this.numero_2);
+            }
+        },
     },
     created: ()=>{
-        console.log("Calculadora creada")
-    }
-
-}
+        console.log("Calculadora creada");
+    },
+};
 </script>
 
 <style>
-
 </style>
