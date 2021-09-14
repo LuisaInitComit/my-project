b67656cf1fd01610009d0aeaabbbdddd49239e72
Luisa Carmona <cp.carmonaclf@gmail.com>
2021-09-14 08:48:11 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <calculadora/>
+  </div>
+</template>
+
+<script>
+import Calculadora from './components/Calculadora.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <h1>Mi primer componente</h1>
+      <!----Interpolación-->
+      <h2>Calculadora</h2>
+      <input type="number" id="num1" placeholder="numero_1"/>
+      <br>
+      <input type="number" ide="num2" placeholder="numero_2"/>
+      <br>
+      <!---<button @click="">Sumar</button>-->
+  </div>
+</template>
+
+<script>
+export default {
+    //datos que manipulará el componente (variables "globales")
+    data: ()=>{
+        return{
+            subtitulo: 'Calculadora',
+            numero_1: null,
+            numero_2: null
+        }
+    },
+    methods:{
+        sumar(){
+            console.log("metodo sumer")
+            console.log("Resultado: ", (numero_1, numero_2))
+        }
+    },
+    created: ()=>{
+        console.log("Calculadora creada")
+    }
+
+}
+</script>
+
+<style>
+
+</style>
